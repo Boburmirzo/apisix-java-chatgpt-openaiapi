ad847707a7d0b693a8bd0ac94570fe57a9d2bdde
Bobur Umurzokov <bobur.umorzokov@astrazeneca.com>
2023-06-14 13:25:14 +0300

    Add Appsmith UI

--- test-curl-cmds.sh
+++ test-curl-cmds.sh
@@ -5,7 +5,7 @@ curl http://localhost:9080/ask-me-anything -X POST -d ' \
 }'
 
 # Claim a new JWT token
-curl -i http://localhost:9080/login?key=user-key -i
+curl -i http://localhost:9080/login?key=appsmithuser@gmail.com
 
 # Test with JWT token
 curl -i http://localhost:9080/ask-me-anything -H 'Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODY4MTAwNjQsImtleSI6InVzZXIta2V5In0.L03lBFPVwthb_r8uo43bizCLnFULRfcodS84FZFn2x8' -X POST -d '
